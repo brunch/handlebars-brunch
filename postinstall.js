a3e3911926cc6679d9e63142ace4b641786e7171
papostol <papostol@kds.com>
2015-11-12 12:24:40 +0100

    Fixed path for runtime.

--- postinstall.js
+++ postinstall.js
@@ -9,14 +9,13 @@ fs.mkdir('./dist');
 
 var runtime = ['', '.runtime'], amd = ['', '.amd'], min = ['', '.min'];
 
-for(var i = 0; i < runtime.length; i++) {
-  for(var j = 0; j < amd.length; j++) {
-    for(var k = 0; k < min.length; k++) {
-      var fileName = 'handlebars' + runtime[i] + amd[j] + min[k] + '.js';
+runtime.forEach(function(r) {
+  amd.forEach(function(a) {
+    min.forEach(function(m) {
+      var fileName = 'handlebars' + r + a + m + '.js';
       var absFilePath = sysPath.join(sysPath.dirname( require.resolve('handlebars')), '..', 'dist', fileName);
       fs.symlinkSync(absFilePath, sysPath.join(__dirname, 'dist', fileName));
-    }
-  }
-}
-
+    });
+  });
+});
 

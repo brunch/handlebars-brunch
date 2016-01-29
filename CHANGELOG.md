3a52f4a697fd10d0b2b0d0c90a1eb77d377dc2a5
Paul Miller <paul@paulmillr.com>
2016-01-29 03:21:26 +0200

    Release 2.0.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# handlebars-brunch 2.0.0 (Jan 29, 2016)
+* Updated source code & API. The plugin would now only work with Brunch 2.2 and higher.
+
 # handlebars-brunch 1.10.1 (12 November 2015)
 * Add license field to package.json
 
--- postinstall.js
+++ postinstall.js
@@ -5,16 +5,24 @@
 var sysPath = require('path');
 var fs = require('fs');
 
-fs.mkdir('./dist');
+var dist = sysPath.join(__dirname, 'dist');
+
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist);
+}
 
 var runtime = ['', '.runtime'], amd = ['', '.amd'], min = ['', '.min'];
+var hPath = sysPath.dirname(require.resolve('handlebars'));
 
 runtime.forEach(function(r) {
   amd.forEach(function(a) {
     min.forEach(function(m) {
       var fileName = 'handlebars' + r + a + m + '.js';
-      var absFilePath = sysPath.join(sysPath.dirname( require.resolve('handlebars')), '..', 'dist', fileName);
-      fs.createReadStream(absFilePath).pipe(fs.createWriteStream(sysPath.join(__dirname, 'dist', fileName)));
+      var absFilePath = sysPath.join(hPath, '..', 'dist', fileName);
+      var file = sysPath.join(__dirname, 'dist', fileName);
+      if (!fs.existsSync(file)) {
+        fs.createReadStream(absFilePath).pipe(fs.createWriteStream(file));
+      }
     });
   });
 });
